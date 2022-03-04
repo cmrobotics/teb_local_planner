02e87fe92272e05de48b6f39ca0d8e69d6a3307e
Erwin Lejeune <erwin.lejeune@cm-robotics.com>
2022-03-04 02:56:19 +0100

    use galactic imports

--- teb_local_planner/include/teb_local_planner/pose_se2.h
+++ teb_local_planner/include/teb_local_planner/pose_se2.h
@@ -48,7 +48,7 @@
 
 #include <tf2/convert.h>
 #include <tf2/utils.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace teb_local_planner
 {
